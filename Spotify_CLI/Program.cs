9e258e1ab80e111072add93bb1ad5bdf03b3c01b
stijn1290 <153715940+stijn1290@users.noreply.github.com>
2025-05-27 16:30:04 +0200

    Een gebruiker heeft een personaliteit en wordt ingelogd als actievegebruiker

--- Spotify_CLI/Program.cs
+++ Spotify_CLI/Program.cs
@@ -1,72 +1,83 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-void menu()
+namespace Spotify_CLI
 {
-    Console.WriteLine("Ben je een admin of normale gebruiker");
-    string option = Console.ReadLine();
-     switch(option)
+    internal class Program
     {
-        case "admin":
-            Console.WriteLine("welkom admin");
-            Console.WriteLine("Wat wil je doen:");
-            Console.WriteLine("1. Artiest aanmaken");
-            Console.WriteLine("2. Nummer aanmaken");
-            Console.WriteLine("3. Album aanmaken");
-            string adminInput = Console.ReadLine();
-            switch(adminInput)
+        private static void Main(string[] args)
+        {
+            List<User> allusers = new List<User>();
+            Person? activeUser = null;
+            string[] names = {"levi", "stijn" };
+            Admin admin = new Admin("admin");
+            for (int i = 0; i < names.Length; i++)
             {
-                case "1": 
-                    break;
-                case "2":
-                    break;
-                case "3":
-                    break;
-                    default:
-                    break;
+                User user = new User(names[i]);
+                allusers.Add(user);
+            }
+            void menu()
+            {
+                string option = "";
+                Console.WriteLine("Ben je een admin of normale gebruiker");
+                option = Console.ReadLine();
+                switch (option)
+                {
+                    case "admin":
+                        setActiveUser(admin);
+                        Console.WriteLine("welkom admin");
+                        Console.WriteLine("Wat wil je doen:");
+                        Console.WriteLine("1. Artiest aanmaken");
+                        Console.WriteLine("2. Nummer aanmaken");
+                        Console.WriteLine("3. Album aanmaken");
+                        string adminInput = Console.ReadLine();
+                        switch (adminInput)
+                        {
+                            case "1":
+                                break;
+                            case "2":
+                                break;
+                            case "3":
+                                break;
+                            default:
+                                break;
 
+                        }
+                        break;
+                    case "gebruiker":
+                        string username = "";
+                        while (!allusers.Any(u => u.name == username))
+                        {
+                            Console.WriteLine("Wat is je gebruikersnaam");
+                            username = Console.ReadLine();
+                        }
+                        Person person = allusers.Find(u => u.name == username);
+                        setActiveUser(person);
+                        Console.WriteLine($"Welkom {getActiveUser().ToString()}");
+                        Console.WriteLine("Wat wil je doen:");
+                        Console.WriteLine("1. Toon alle nummers");
+                        Console.WriteLine("2. Toon alle albums");
+                        string userInput = Console.ReadLine();
+                        switch (userInput)
+                        {
+                            case "1":
+                                break;
+                            case "2":
+                                break;
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("verkeerde input");
+                        break;
+                }
             }
-            break;
-        case "gebruiker":
-            Console.WriteLine("Wat is je gebruikersnaam");
-            string username = Console.ReadLine();
-            switch(username)
+            void setActiveUser(Person person)
             {
-                case "levi":
-                    Console.WriteLine($"Welkom {username}");
-                    Console.WriteLine("Wat wil je doen:");
-                    Console.WriteLine("1. Toon alle nummers");
-                    Console.WriteLine("2. Toon alle albums");
-                    string userInput = Console.ReadLine();
-                    switch(userInput)
-                    {
-                        case "1":
-                            break;
-                        case "2":
-                            break;
-                    }
-                    break;
-                case "stijn":
-                    Console.WriteLine($"Welkom {username}");
-                    Console.WriteLine("Wat wil je doen:");
-                    Console.WriteLine("1. Toon alle nummers");
-                    Console.WriteLine("2. Toon alle albums");
-                    string user2Input = Console.ReadLine();
-                    switch (user2Input)
-                    {
-                        case "1":
-                            break;
-                        case "2":
-                            break;
-                    }
-                    break;
-                default:
-                    break;
+                activeUser = person;
             }
-            break;
-        default:
-            Console.WriteLine("verkeerde input");
-            break;
+            Person getActiveUser()
+            {
+                return activeUser;
+            }
+            menu();
+        }
     }
 }
-
-menu();
